b92c8a95bcc449df0daac7818136fbf966d919b5
Haovodoi2004 <haovodoi2004@gmail.com>
2025-03-12 22:44:40 +0700

    quản lý đơn hàng user

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
-import App from "./App";
+import App from "./src/screens/ChitietdonhangUser";
 
 AppRegistry.registerComponent(appName, () => App);
