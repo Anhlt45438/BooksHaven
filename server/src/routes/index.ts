6e51997af198893d26e6a430d2f3b222f5395021
Đào Công Kha <leeminhovn2k4@gmail.com>
2025-03-14 11:09:34 +0700

    add cart router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,7 @@ import booksRouter from "./books.routes";
 import rolesRouter from "./roles.routers";
 import shopRouter from "./shop.routes";
 import categoriesRouter from "./categories.routes";
+import cartRouter from "./cart.routes";
 
 const routersApp = Router();
 routersApp.use("/users", usersRouter);
@@ -11,6 +12,8 @@ routersApp.use("/books", booksRouter);
 routersApp.use("/roles", rolesRouter);
 routersApp.use('/shops', shopRouter);
 routersApp.use('/categories', categoriesRouter);
+routersApp.use('/cart', cartRouter);
+
 
 
 export default routersApp;
