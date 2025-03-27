f61e404cc7fcb9b4f1e6c8c0af788428f83fa122
Haovodoi2004 <haovodoi2004@gmail.com>
2025-03-27 13:24:53 +0700

    Chỉnh sửa và bổ sung giao diện rút tiền cho shop

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
-import App from "./App";
+import App from "./src/screens/Ruttien3";
 
 AppRegistry.registerComponent(appName, () => App);
