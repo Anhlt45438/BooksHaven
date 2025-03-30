115a56afc93f64bfc699830944774a68f25ab4f5
Đào Công Kha <leeminhovn2k4@gmail.com>
2025-03-30 20:41:25 +0700

    add mail

--- server/src/controllers/users.controllers.ts
+++ server/src/controllers/users.controllers.ts
@@ -5,7 +5,7 @@ import usersServices from "~/services/users.services";
 import { getUserRolesHelper } from "./roles.controller";
 import databaseServices from "~/services/database.services";
 import { signJwt, verifyToken } from "~/untils/jwt";
-import nodemailer from "nodemailer";
+const nodemailer = require('nodemailer')
 import { config } from "dotenv";
 import { hasPassword } from "~/untils/crypto";
 config();
