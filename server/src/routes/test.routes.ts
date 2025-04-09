928807d91fad882d86b0b01f5010748ff4d7a97b
Đào Công Kha <leeminhovn2k4@gmail.com>
2025-04-09 23:41:03 +0700

    add controller

--- server/src/routes/test.routes.ts
+++ server/src/routes/test.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { convertStringToInt } from '~/controllers/test.controllers';
+import { convertStringToInt, testingApp } from '~/controllers/test.controllers';
 
 const testRouter = Router();
 
 testRouter.post('/fix-type-database-field', convertStringToInt);
-testRouter.post('/bus-block-puzzle',);
+testRouter.post('/bus-block-puzzle',testingApp);
 export default testRouter;
