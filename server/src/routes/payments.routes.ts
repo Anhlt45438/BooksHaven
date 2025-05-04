e4a15e5caa0b6ed5e3b66ebc32a5f448a52f40b8
Đào Công Kha <leeminhovn2k4@gmail.com>
2025-05-04 11:32:17 +0700

    fix rotute payment

--- server/src/routes/payments.routes.ts
+++ server/src/routes/payments.routes.ts
@@ -7,13 +7,27 @@ import {
   getPaymentById
 } from '~/controllers/payment.controller';
 import { authMiddleware } from '~/middlewares/auth.middleware';
+import { validateCreateOrder } from '~/middlewares/orders.middleware';
+import { paymentValidator, vnPayValidator } from '~/middlewares/payment.middleware';
 
 const paymentsRouter = Router();
 
-paymentsRouter.post('/calculate-total-amount', authMiddleware, calculateOrderTotal);
+paymentsRouter.post('/calculate', authMiddleware, calculateOrderTotal);
 paymentsRouter.post('/create-payment-url', authMiddleware, createPaymentUrlController);
 paymentsRouter.get('/vnpay-return', vnpayReturnController);
 paymentsRouter.get('/history', authMiddleware, getPaymentHistory);
 paymentsRouter.get('/:paymentId', authMiddleware, getPaymentById);
-
+paymentsRouter.post(
+  '/calculate-total-amount',
+  authMiddleware,
+  paymentValidator,
+  calculateOrderTotal
+);
+paymentsRouter.get("/vnpay-return", vnpayReturnController);
+paymentsRouter.post('/create-vnpay-payment', 
+  authMiddleware, 
+  vnPayValidator,
+  validateCreateOrder,
+  createPaymentUrlController
+);
 export default paymentsRouter;
